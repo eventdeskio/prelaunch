3fa23c199d9b8ecd98ffd6b9d740eda8aa87a2f2
harishhari130615 <harish130615@gmail.com>
2025-02-25 06:37:00 +0530

    final

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://devb.eventdesk.io',
+  apiUrl : 'https://eventdesk.io',
   POSTHOG_KEY: 'phc_lzRnYl7IDA5MW0wNt3mddz9t2BYBZX0bNWgypBqM9gx',
   POSTHOG_HOST: 'https://us.i.posthog.com',
   // siteKey: '6Le7v98qAAAAAC7nC-GXj-nhSX_skNBR-Ql2IF8j',
